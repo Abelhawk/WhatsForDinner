0a17711bfaa934b0d823940bfae845c448d4b7fc
Austin Ballard <alballard@verisk.com>
2020-07-08 09:39:22 -0600

    Consolidation!

--- triggers.js
+++ triggers.js
@@ -5,91 +5,49 @@ let dessertsJa = [];
 let breakfastsJa = [];
 let snacksJa = [];
 
-function randomDinner() {
-    let meal = "";
+function randomSelection(type) {
+    let meal = '';
     let number = -1;
     let newRecipe = false;
-    if (dinnersJa.length !== dinners.length) {
-        while (!newRecipe) {
-            number = rando(dinners.length);
-            newRecipe = true;
-            for (let i = 0; i < dinnersJa.length; i++) {
-                if (dinnersJa[i] === number) {
-                    newRecipe = false;
-                }
-            }
-        }
-        meal = dinners[number];
-        dinnersJa.push(number);
-        textField.html(`<p>How about ${meal}?</p>`);
-    } else {
-        textField.html(`<p class="smaller">Well, I can't think of anything else. Maybe go out to eat or flip through a cookbook?</p>`);
+    let arrayJa = [];
+    let foods;
+    let outOfIdeas = "Welp, I'm out of ideas.";
+    switch (type) {
+        case 'dinner':
+            arrayJa = dinnersJa;
+            foods = dinners;
+            outOfIdeas = "Well, I can't think of anything else. Maybe go out to eat or flip through a cookbook?";
+            break;
+        case 'dessert':
+            arrayJa = dessertsJa;
+            foods = desserts;
+            outOfIdeas = "Well, I can't think of anything else. Maybe go out to eat or flip through a cookbook?";
+            break;
+        case 'breakfast':
+            arrayJa = breakfastsJa;
+            foods = breakfasts;
+            outOfIdeas = "Well, I can't think of anything else. Maybe just have cereal?";
+            break;
+        case 'snack':
+            arrayJa = snacksJa;
+            foods = snacks;
+            outOfIdeas = "Well, I'm out of ideas. Maybe just buy a bag of chips?";
     }
-}
-
-function randomDessert() {
-    let meal = "";
-    let number = -1;
-    let newRecipe = false;
-    if (dessertsJa.length !== desserts.length) {
+    if (arrayJa.length !== foods.length) {
         while (!newRecipe) {
-            number = rando(desserts.length);
+            number = rando(foods.length);
             newRecipe = true;
-            for (let i = 0; i < dessertsJa.length; i++) {
-                if (dessertsJa[i] === number) {
+            for (let i = 0; i < arrayJa.length; i++) {
+                if (arrayJa[i] === number) {
                     newRecipe = false;
                 }
             }
         }
-        meal = desserts[number];
-        dessertsJa.push(number);
+        meal = foods[number];
+        arrayJa.push(number);
         textField.html(`<p>How about ${meal}?</p>`);
     } else {
-        textField.html(`<p class="smaller">Well, I can't think of anything else. Maybe go to a bakery or flip through a cookbook?</p>`);
-    }
-}
-
-function randomBreakfast() {
-    let meal = "";
-    let number = -1;
-    let newRecipe = false;
-    if (breakfastsJa.length !== breakfasts.length) {
-        while (!newRecipe) {
-            number = rando(breakfasts.length);
-            newRecipe = true;
-            for (let i = 0; i < breakfastsJa.length; i++) {
-                if (breakfastsJa[i] === number) {
-                    newRecipe = false;
-                }
-            }
-        }
-        meal = breakfasts[number];
-        breakfastsJa.push(number);
-        textField.html(`<p>How about ${meal}?</p>`);
-    } else {
-        textField.html(`<p class="smaller">Well, I can't think of anything else. Maybe just have cereal?</p>`);
-    }
-}
-
-function randomSnack() {
-    let meal = "";
-    let number = -1;
-    let newRecipe = false;
-    if (snacksJa.length !== snacks.length) {
-        while (!newRecipe) {
-            number = rando(snacks.length);
-            newRecipe = true;
-            for (let i = 0; i < snacksJa.length; i++) {
-                if (snacksJa[i] === number) {
-                    newRecipe = false;
-                }
-            }
-        }
-        meal = snacks[number];
-        snacksJa.push(number);
-        textField.html(`<p>How about ${meal}?</p>`);
-    } else {
-        textField.html(`<p class="smaller">Well, I'm out of ideas. Maybe just buy a bag of chips?</p>`);
+        textField.html(`<p class="smaller">${outOfIdeas}</p>`);
     }
 }
 
