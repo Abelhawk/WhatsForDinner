be844bf8383c8ce281fb29566759981e21e80288
Austin Ballard <aballard@prodriver.com>
2023-12-05 09:19:23 -0600

    Added some new recipes and axed a few that I just never crave.

--- recipes.js
+++ recipes.js
@@ -6,16 +6,15 @@ let dinners = [
     "Brasilian lasanha", "burger dogs", "cachorro quente", "pizza", "gnocchi",
     "chicken fried steak", "chicken noodle soup", "chicken pot pie", "shepherd's pie",
     "chili baked potato", "espetinhos", "tacos", "sweet pulled pork", "oven nachos",
-    "homemade burgers and buns", "sliders", "corned beef and Irish soda bread",
+    "hamburgers", "sliders", "corned beef and Irish soda bread", "Thai curry",
     "bean dip", "tuna mac", "tuna melts", "chicken nuggets", "chicken sandwiches",
-    "meatball subs", "rib-a-cue sandwiches", "curry", "twice-baked potatoes",
+    "meatball subs", "rib-a-cue sandwiches", "chicken tikka masala", "twice-baked potatoes",
     "zucchini soup", "meatloaf", "white-bean chicken chili", "sausage-pepper pasta",
-    "hosanna stew", "Greek lemon chicken soup", "CostCo rotisserie chicken",
-    "ham and funeral potatoes", "baked ham & cheese", "Tasty fajita pasta", "fajitas",
+    "hosanna stew", "Greek lemon chicken soup", "CostCo rotisserie chicken", "fettucine alfredo",
+    "ham and funeral potatoes", "hammies", "fajita pasta", "fajitas", "buttered noodles",
     "classic chili", "beef stew", "manicotti", "carbonara", "chicken salad sandwiches",
-    "taco salad", "chef salad", "Chicken Penne Doolittle", "macaroni & cheese-burger",
-    "chili cheese fries", "enchiladas", "tetrazinni", "BLT street tacos", "buttered noodles",
-    "hot dog boats", "BLTs", "breakfast for dinner", "dutch oven meat and spuds", "fettucine alfredo",
+    "taco salad", "chef salad", "chili cheese fries", "enchiladas", "tetrazinni", "BLT street tacos",
+    "hot dog boats", "BLTs", "you click the breakfast button and try one of those", "dutch oven meat and spuds", 
     "Chinese Sundaes", "crunch wraps", "Missionary Surprise", "Dad's ground beef stew",
     "Ramen noodles", "sweet and sour chicken on rice", "Salisbury steak", "grilled cheese and tomato soup",
     "macaroni and cheese", "Sloppy Joes", "potato soup", "chili dogs", "French dip",
@@ -25,7 +24,8 @@ let dinners = [
     "mushroom pork chops on rice", "tuna salad pitas", "pesto pasta", "beef bulgogi",
     "bolognese", "Buddha bowls", "fish pie", "rosemary lemon chicken", "cowboy spud",
     "zuppa toscana", "buffalo wings", "calzones", "Navajo tacos", "Hungarian goulash",
-    "wontons",
+    "potstickers", "chicken cordon blue", "malibu chicken", "carne asada burritos", "borscht",
+    "souvlaki", "broccoli beef"
 ];
 
 let breakfasts = [
@@ -39,7 +39,7 @@ let breakfasts = [
     "yogurt granola parfaits", "oatmeal", "Fūl Medames", "huevos rancheros",
     "cuscuz", "breakfast BLTs", "donuts", "avocado toast", "hash browns",
     "pumpkin waffles", "breakfast casserole", "scones with honey butter",
-    "a pork patty sandwich", "breakfast ramen", "overnight oats"
+    "a pork patty sandwich", "breakfast ramen", "overnight oats", "autumnbrosia"
 ];
 
 let desserts = [
@@ -64,22 +64,24 @@ let desserts = [
     "caramel corn", "frosted sugar cookies", "slush", "angel food cake",
     "buckeyes", "cobbler", "pumpkin roll", "monkey bread", "Sandy Sherbet Pie",
     "brigadeiro", "beijinhos", "gaumy popcorn", "lime frappé", "mango rice",
-    "pecan pie", "eggnog", "mud pudding", "bread pudding", "chocolate mousse"
+    "pecan pie", "eggnog", "mud pudding", "bread pudding", "chocolate mousse",
+    "Hello Dollies", "sconuts", "cheesecake cookies"
 ];
 
 let snacks = [
     "potato salad", "rhubarb muffins", "lemon poppy seed muffins", "cheeseball and crackers",
-    "Texas caviar", "ramen salad", "Caesar salad", "jalapeño poppers", "rattlesnake bites",
-    "a cactus blossom", "oven-baked potato wedges", "seven-layer dip", "pizza bites", "broccoli salad", "grilled corn on the cob",
+    "Texas caviar", "ramen salad", "Caesar salad", "jalapeño poppers", "rattlesnake bites", "Mexican street corn",
+    "a cactus blossom", "oven-baked potato wedges", "seven-layer dip", "pizza bites", "broccoli salad", 
     "BYU-I spinach salad", "pasta salad", "coleslaw", "baked beans", "pão de queijo", "grape Jello salad",
-    "Dot's pretzels", "soft pretzels", "spinach dip", "artichoke dip", "chips and dip", "crackers and cheese",
+    "Dot's pretzels", "soft pretzels", "spinach artichoke dip", "artichoke dip", "chips and dip", "crackers and cheese",
     "twice-baked potatoes", "mashed potatoes", "cheese sticks", "breadsticks", "fondue", "cheddar biscuits",
     "roasted chickpeas", "hummus", "chips and salsa", "Li'l Smokies", "pigs in a blanket", "onion dip",
     "taquitos", "Chex party mix", "wantons", "scones", "strawberry lemonade", "chicken nuggets", "buffalo chicken dip",
-    "buffalo wings", "cucumber sandwiches", "sweet and sour meatballs", "tortilla roll-ups", "chips and guacamole",
+    "buffalo wings", "cucumber sandwiches", "soy garlic meatballs", "tortilla roll-ups", "chips and guacamole",
     "macaroni and cheese", "green bean casserole", "sweet potato casserole", "deviled eggs", "risotto",
     "roasted vegetables", "roasted potatoes", "asparagus", "tater tots", "fries", "garlic bread", "hush puppies",
     "fondant potatoes", "fried rice", "Spanish rice", "au gratin potatoes", "rolls", "corn bread", "onion rings",
     "sunshine carrots", "Hungarian cucumber salad", "mini corn dogs", "popcorn", "potato skins", "nachos",
-    "bean dip"
+    "bean dip", "corn fritters", "scallion pancakes", "cream cheese wantons", "chips and queso",
+    "roasted asparagus"
 ];
